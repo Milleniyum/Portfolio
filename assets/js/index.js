87f84ef5de4230b542d11dfdeac364532f7eca70
JDLong <jdlong1980@gmail.com>
2019-03-26 12:13:04 -0400

    updated opening animation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,12 @@
-var startImages = ["head_2.png", "head_3.png", "head_1.png", "head_4.png", "head_5_white.png"];
+var startImages = ["head_2.png", "head_3.png", "head_1.png", "head_4.png", "head_5.png"];
 var head = 0;
 var intervalHeadAnimation = setInterval(animateHead, 1000);
 
 function animateHead() {
-    if (head == 4) clearInterval(intervalHeadAnimation);
     $("#start-image").attr("src", "assets/images/" + startImages[head]);
-    head++;
+    if (head == 4) {
+        clearInterval(intervalHeadAnimation);
+    } else {
+        head++;
+    }
 }
